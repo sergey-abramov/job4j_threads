a06b8f3f4ccf5ef5ccf7299d5cecb7ddb73c9578
LESHA <belochuvach@gmail.com>
2023-02-27 18:22:53 +0300

    Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+        }
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+    }
+}
+
