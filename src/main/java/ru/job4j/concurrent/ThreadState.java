568f6350b2fad6bbe9b2655a3a224f210ac794ad
LESHA <belochuvach@gmail.com>
2023-02-28 10:57:37 +0300

    Состояние нити. [#229175 #395704]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -13,7 +13,7 @@ public class ThreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-                && second.getState() != Thread.State.TERMINATED) {
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getState());
         }
         System.out.println(first.getState());
