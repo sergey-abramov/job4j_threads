7e96f667a61da84bfe9febe1eaed4cd648b1855a
LESHA <belochuvach@gmail.com>
2023-03-01 18:17:52 +0300

    JCIP. Настройка библиотеки [#268575]

--- src/main/java/ru/job4j/DCLSingleton.java
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -1,11 +1,17 @@
 package ru.job4j;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class DCLSingleton {
+
     private static volatile DCLSingleton inst;
 
     private DCLSingleton() {
     }
 
+    @GuardedBy("DCLSingleton.class")
     public static DCLSingleton instOf() {
         if (inst == null) {
             synchronized (DCLSingleton.class) {
