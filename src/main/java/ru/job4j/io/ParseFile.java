39d877b45c66690b4221391650dadafec225aa40
LESHA <belochuvach@gmail.com>
2023-03-01 10:48:50 +0300

    Visibility. Общий ресурс вне критической секции [#1102 #396248]

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -22,7 +22,7 @@ public class ParseFile {
         StringBuilder output = new StringBuilder();
         try (InputStream i = new FileInputStream(file)) {
             int data;
-            while ((data = i.read()) > 0) {
+            while ((data = i.read()) != -1) {
                 if (predicate.test(data)) {
                     output.append((char) data);
                 }
