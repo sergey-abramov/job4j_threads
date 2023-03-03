c3f093a065cf098bef7d28525b5d3a4b093a11a6
LESHA <belochuvach@gmail.com>
2023-03-03 22:06:36 +0300

    Обеспечить остановку потребителя. [#66825 #397255]

--- src/main/java/ru/job4j/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -48,4 +48,5 @@ public class SimpleBlockingQueue<T> {
             return queue.isEmpty();
         }
     }
+
 }
--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -36,4 +36,5 @@ public class ParallelSearch {
         consumer.interrupt();
         consumer.join();
     }
+
 }
